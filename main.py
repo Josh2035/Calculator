bc589cf28c26ac5fbaf3414733a357a82aa974ab
Josh2035 <88112012+Josh2035@users.noreply.github.com>
2022-06-06 17:21:06 -0600

    shorter button abbreviation

--- main.py
+++ main.py
@@ -4,44 +4,45 @@ import time
 # with Tkinter buttons or at least with the list comprehension I'm using
 
 
-def b1(): e1.insert("end", '*')
-def b2(): e1.insert("end", '7')
-def b3(): e1.insert("end", '8')
-def b4(): e1.insert("end", '9')
-def b5(): e1.insert("end", '+')
-def b6(): e1.insert("end", '4')
-def b7(): e1.insert("end", '5')
-def b8(): e1.insert("end", '6')
-def b9(): e1.insert("end", '-')
-def b10(): e1.insert("end", '1')
-def b11(): e1.insert("end", '2')
-def b12(): e1.insert("end", '3')
-def b13(): e1.insert("end", '/')
-def b14(): e1.insert("end", '0')
-def b15(): e1.insert("end", '.')
+def b1(): en1.insert("end", '*')
+def b2(): en1.insert("end", '7')
+def b3(): en1.insert("end", '8')
+def b4(): en1.insert("end", '9')
+def b5(): en1.insert("end", '+')
+def b6(): en1.insert("end", '4')
+def b7(): en1.insert("end", '5')
+def b8(): en1.insert("end", '6')
+def b9(): en1.insert("end", '-')
+def b10(): en1.insert("end", '1')
+def b11(): en1.insert("end", '2')
+def b12(): en1.insert("end", '3')
+def b13(): en1.insert("end", '/')
+def b14(): en1.insert("end", '0')
+def b15(): en1.insert("end", '.')
 
 
 def equal():
     try:
-        answer = e1.get()
-        e1.delete(0, 'end')
-        e1.insert("end", eval(answer))
+        answer = en1.get()
+        en1.delete(0, 'end')
+        en1.insert("end", eval(answer))
     except:
-        e1.delete(0, 'end')
-        e1.insert('end', 'ERROR')
-        e1.delete(0, 'end')
+        en1.delete(0, 'end')
+        en1.insert('end', 'ERROR')
+        en1.delete(0, 'end')
 
 
 root = tk.Tk()
 root.geometry("207x355")
 root.title("calc")
 
-# e1 is the equal
-e1 = tk.Entry(root, width=25)
-e1.grid(row=0, column=0, columnspan=3)
+# en1 is the Entry
+en1 = tk.Entry(root, width=25)
+en1.grid(row=0, column=0, columnspan=3)
 
-Equal = tk.Button(root, text='=', width=6, height=4, command=equal)
-Equal.grid(row='4', column='3')
+#
+eq1 = tk.Button(root, text='=', width=6, height=4, command=equal)
+eq1.grid(row='4', column='3')
 
 buttons = ['*', '7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '/', '.']
 rows = ['0', '1', '1', '1', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4']
