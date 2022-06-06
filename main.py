5088bf5b6a6fefa5dec5575881ed578bd66f5214
Josh2035 <88112012+Josh2035@users.noreply.github.com>
2022-06-06 17:34:15 -0600

    3 new buttons

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import time
 # Defining Buttons (Maybe could do this in a loop to make it easier, but it seems the lambda:add() command doesn't work
 # with Tkinter buttons or at least with the list comprehension I'm using
 
@@ -19,6 +18,9 @@ def b12(): en1.insert("end", '3')
 def b13(): en1.insert("end", '/')
 def b14(): en1.insert("end", '0')
 def b15(): en1.insert("end", '.')
+def b16(): en1.delete(0, 'end')
+def b17(): en1.insert('end', '(')
+def b18(): en1.insert('end', ')')
 
 
 def equal():
@@ -33,22 +35,22 @@ def equal():
 
 
 root = tk.Tk()
-root.geometry("207x355")
-root.title("calc")
+root.geometry("259x355")
+root.title("calculator")
 
 # en1 is the Entry
 en1 = tk.Entry(root, width=25)
 en1.grid(row=0, column=0, columnspan=3)
 
-#
-eq1 = tk.Button(root, text='=', width=6, height=4, command=equal)
-eq1.grid(row='4', column='3')
+# eq1 is the Equal
+eq1 = tk.Button(root, text='=', width=13, height=4, command=equal)
+eq1.grid(row='4', column='3', columnspan=2)
 
-buttons = ['*', '7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '/', '.']
-rows = ['0', '1', '1', '1', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4']
-columns = ['3', '0', '1', '2', '3', '0', '1', '2', '3', '0', '1', '2', '3', '2']
-buttons2 = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10', 'b11', 'b12', 'b13', 'b15']
-functions = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b15]
+buttons = ['*', '7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '/', '.', 'AC', '(', ')']
+rows = ['0', '1', '1', '1', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4', '0', '1', '2']
+columns = ['3', '0', '1', '2', '3', '0', '1', '2', '3', '0', '1', '2', '3', '2', '4', '4', '4']
+buttons2 = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10', 'b11', 'b12', 'b13', 'b15', 'b16', 'b17', b'18']
+functions = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b15, b16, b17, b18]
 
 for x, y, z, t, f in zip(buttons, rows, columns, buttons2, functions):
     t = tk.Button(root, text=x, width=6, height=4, command=f)
